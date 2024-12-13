c371de93ae29239f849a8c8864457bb7f9d1bcf4
osama-lakhan <osamayaseenlakhan@gmail.com>
2024-12-13 21:38:38 +0500

    packages updated

--- src/app/components/general/general.module.ts
+++ src/app/components/general/general.module.ts
@@ -1,38 +1,39 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component'
-import { HeaderComponent } from './header/header.component'
-import { NgbModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { RouterModule } from "@angular/router";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { HttpClient } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import {
+  NgbDropdownModule,
+  NgbModule,
+  NgbNavModule,
+} from "@ng-bootstrap/ng-bootstrap";
 
-export function HttpLoaderFactory(http: HttpClient){
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent
-  ],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
-    NgbModule,
+    NgbNavModule,
     RouterModule,
     NgbDropdownModule,
     FormsModule,
     ReactiveFormsModule,
     TranslateModule.forChild({
       loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-    })
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent],
 })
-export class GeneralModule { }
+export class GeneralModule {}
