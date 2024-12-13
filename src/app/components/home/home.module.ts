664868cea09977a5cf906885f79abd709cda2f04
osama-lakhan <osamayaseenlakhan@gmail.com>
2024-12-13 19:12:39 +0500

    feat & fix: experiences added & some fixes done

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,23 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
-import { AboutComponent } from './about/about.component';
-import { BannerComponent } from './banner/banner.component';
-import { ContactComponent } from './contact/contact.component';
-import { JobsComponent } from './jobs/jobs.component';
-import { MoreProyectsComponent } from './more-proyects/more-proyects.component';
-import { ProyectsComponent } from './proyects/proyects.component';
-import { NgbModule, NgbNav, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { CarouselModule } from 'ngx-owl-carousel-o';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./home.component";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { HttpClient } from "@angular/common/http";
+import { AboutComponent } from "./about/about.component";
+import { BannerComponent } from "./banner/banner.component";
+import { ContactComponent } from "./contact/contact.component";
+import { JobsComponent } from "./jobs/jobs.component";
+import { NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
+import { CarouselModule } from "ngx-owl-carousel-o";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { projectsComponent } from "./projects/projects.component";
 
-export function HttpLoaderFactory(http: HttpClient){
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
-
-
 
 @NgModule({
   declarations: [
@@ -25,8 +22,7 @@ export function HttpLoaderFactory(http: HttpClient){
     BannerComponent,
     AboutComponent,
     JobsComponent,
-    ProyectsComponent,
-    MoreProyectsComponent,
+    projectsComponent,
     ContactComponent,
   ],
   imports: [
@@ -35,11 +31,11 @@ export function HttpLoaderFactory(http: HttpClient){
     CarouselModule,
     TranslateModule.forChild({
       loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-    })
-  ]
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/components/home/projects/projects.component.spec.ts
+++ src/app/components/home/projects/projects.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import { MoreProyectsComponent } from './more-proyects.component';
+import { projectsComponent } from './projects.component';
 
-describe('MoreProyectsComponent', () => {
-  let component: MoreProyectsComponent;
-  let fixture: ComponentFixture<MoreProyectsComponent>;
+describe('projectsComponent', () => {
+  let component: projectsComponent;
+  let fixture: ComponentFixture<projectsComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ MoreProyectsComponent ]
+      declarations: [ projectsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MoreProyectsComponent);
+    fixture = TestBed.createComponent(projectsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
